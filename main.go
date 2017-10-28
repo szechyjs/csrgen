873b74472e5915f078179c1cc7272b6a9bf303be
Jared Szechy <jared.szechy@gmail.com>
2017-10-28 18:37:24 -0400

    Add license

--- main.go
+++ main.go
@@ -1,3 +1,21 @@
+/*
+csrgen - Tool for creating certificate signing requests (CSRs)
+Copyright (C) 2017 Jared Szechy
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package main
 
 import (
@@ -186,6 +204,9 @@ func init() {
 }
 
 func main() {
+    fmt.Println("csrgen  Copyright (C) 2017  Jared Szechy")
+    fmt.Println("This program comes with ABSOLUTELY NO WARRANTY")
+
 	flag.Parse()
 
 	prefs := Preferences{}
